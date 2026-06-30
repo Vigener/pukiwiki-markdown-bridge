7fdc90d7a72f71599f14243c8066b7d39f085f5c
Mikoto Igarashi <s2310970@u.tsukuba.ac.jp>
2026-07-01 01:05:44 +0900

    chore: add .gitignore and remove tracked build/test files

--- dist/assets/content_script.ts-BgdTgqYy.js
+++
@@ -1,2 +0,0 @@
-(function(){chrome.runtime.onMessage.addListener((t,u,r)=>{const e=document.querySelector('textarea[name="msg"]');return e?(t.type==="GET_TEXT"?r({text:e.value}):t.type==="SET_TEXT"&&(e.value=t.text,e.dispatchEvent(new Event("input",{bubbles:!0})),e.dispatchEvent(new Event("change",{bubbles:!0})),r({success:!0})),!1):(r({error:"Not found"}),!1)});
-})()
